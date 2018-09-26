2f8dfbc550f00954ce0b1a305f8c0744ff24004a
candypoplatte <hwanght1@gmail.com>
2018-09-26 12:41:24 +0900

    Add timestamped, image, comment 모델 추가
    Signed-off-by: candypoplatte <hwanght1@gmail.com>

--- instagram_clone/images/models.py
+++ instagram_clone/images/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Image(TimeStampedModel):
+    file = models.ImageField()
+    location = models.CharField(max_length=140)
+    caption = models.TextField()
+
+
+class Comment(TimeStampedModel):
+    message = models.TextField()
