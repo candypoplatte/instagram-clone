b7b7efd525babdf7a34e234bcb9ef8592092031c
candypoplatte <hwanght1@gmail.com>
2018-09-26 14:16:40 +0900

    Add ListAllImages 시리얼라이저 추가
    Signed-off-by: candypoplatte <hwanght1@gmail.com>

--- instagram_clone/images/views.py
+++ instagram_clone/images/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from instagram_clone.images.models import Image
+from instagram_clone.images import serializers
+
+
+class ListAllImages(APIView):
+    def get(self, request, format=None):
+        all_images = Image.objects.all()
+        serializer = serializers.ImageSerializer(all_images, many=True)
+        return Response(data=serializer.data)
