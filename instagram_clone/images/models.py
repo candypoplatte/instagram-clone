7c383c8ac4cf1406e28b7d4c510b83f94f13e501
candypoplatte <hwanght1@gmail.com>
2018-09-26 13:32:29 +0900

    Modify comment, like, image model에 외래키 필드 추가(creator, image)
    Signed-off-by: candypoplatte <hwanght1@gmail.com>

--- instagram_clone/images/models.py
+++ instagram_clone/images/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models import SET_NULL
+
+from instagram_clone.users.models import User
 
 
 class TimeStampedModel(models.Model):
@@ -10,10 +13,23 @@ class TimeStampedModel(models.Model):
 
 
 class Image(TimeStampedModel):
+    """Image Model"""
+
     file = models.ImageField()
     location = models.CharField(max_length=140)
     caption = models.TextField()
+    creator = models.ForeignKey(User, null=True, on_delete=SET_NULL)
 
 
 class Comment(TimeStampedModel):
+    """Comment Model"""
+
     message = models.TextField()
+    creator = models.ForeignKey(User, null=True, on_delete=SET_NULL)
+    image = models.ForeignKey(Image, null=True, on_delete=SET_NULL)
+
+
+class Like(TimeStampedModel):
+    """Like Model"""
+
+    creator = models.ForeignKey(User, null=True, on_delete=SET_NULL)
