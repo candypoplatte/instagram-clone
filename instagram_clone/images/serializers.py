13c4378fb92c86ae1fd7719318d5635e274cb893
candypoplatte <hwanght1@gmail.com>
2018-09-26 14:52:31 +0900

    Add image 시리얼라이저에서 comment_set, like_set을 불러오도록 수정
    Signed-off-by: candypoplatte <hwanght1@gmail.com>

--- instagram_clone/images/serializers.py
+++ instagram_clone/images/serializers.py
@@ -2,23 +2,29 @@ from rest_framework import serializers
 from . import models
 
 
-class ImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Image
-        fields = '__all__'
-
-
 class CommentSerializer(serializers.ModelSerializer):
-    image = ImageSerializer()
-
     class Meta:
         model = models.Comment
         fields = '__all__'
 
 
 class LikeSerializer(serializers.ModelSerializer):
-    image = ImageSerializer()
-
     class Meta:
         model = models.Like
         fields = '__all__'
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    comment_set = CommentSerializer(many=True)
+    like_set = LikeSerializer(many=True)
+
+    class Meta:
+        model = models.Image
+        fields = (
+            'file',
+            'location',
+            'caption',
+            'creator',
+            'comment_set',
+            'like_set',
+        )
