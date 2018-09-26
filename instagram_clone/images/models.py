bbf8b74c8a6945a54a452d0383cd836bddec44ae
candypoplatte <hwanght1@gmail.com>
2018-09-26 13:46:20 +0900

    Add Image, Comment, Like 모델에 string representation 추가
    Signed-off-by: candypoplatte <hwanght1@gmail.com>

--- instagram_clone/images/models.py
+++ instagram_clone/images/models.py
@@ -20,6 +20,9 @@ class Image(TimeStampedModel):
     caption = models.TextField()
     creator = models.ForeignKey(User, null=True, on_delete=SET_NULL)
 
+    def __str__(self):
+        return f'{self.location} -{self.caption}'
+
 
 class Comment(TimeStampedModel):
     """Comment Model"""
@@ -28,9 +31,15 @@ class Comment(TimeStampedModel):
     creator = models.ForeignKey(User, null=True, on_delete=SET_NULL)
     image = models.ForeignKey(Image, null=True, on_delete=SET_NULL)
 
+    def __str__(self):
+        return self.message
+
 
 class Like(TimeStampedModel):
     """Like Model"""
 
     creator = models.ForeignKey(User, null=True, on_delete=SET_NULL)
     image = models.ForeignKey(Image, null=True, on_delete=SET_NULL)
+
+    def __str__(self):
+        return f'User : {self.creator.username} - Image caption : {self.image.caption}'
